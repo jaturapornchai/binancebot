84b16d3559cc94eac9e7b4f92c4ede61969a5f14
jaturapornchai <jaturapornchai@gmail.com>
2025-03-08 22:08:28 +0700

    zzz

--- app.py
+++ app.py
@@ -264,7 +264,8 @@ class GateIOLinearRegressionChannelScanner:
                                 
                                 signal = None
                                 if high_touches_T and is_green and latest_price > channel['T']:
-                                    signal = "LONG"
+                                    signal = None
+                                    # signal = "LONG"
                                 elif low_touches_B and is_red and latest_price < channel['B']:
                                     signal = "SHORT"
                                 
